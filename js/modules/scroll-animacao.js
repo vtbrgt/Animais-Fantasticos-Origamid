544ae097a51969ea3393d9c5ace797584273b4a9
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2022-08-08 20:20:23 -0300

    using modules

---
+++ js/modules/scroll-animacao.js
@@ -0,0 +1,17 @@
+/* Animação ao scroll */
+export default function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add('ativo');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', animaScroll);
+}
