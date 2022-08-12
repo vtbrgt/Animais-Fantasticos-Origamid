189ab9c945ed20c35dc2eb7bf1be53577c34bca8
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-08-12 00:31:56 -0300

    bug fix

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,23 +1,22 @@
-import outsideClick from './outsideClick.js'
+import outsideClick from './outsideclick.js';
 
 export default function initMenuMobile() {
-    const menuButton = document.querySelector('[data-menu="button"]')
-    const menuList = document.querySelector('[data-menu="list"]')
-    const eventos = ['click', 'touchstart']
-    
-    if(menuButton) {
-        function openMenu(event) {
-            menuList.classList.add('active')
-            menuButton.classList.add('active')
-            outsideClick(menuList, eventos, () => {
-                menuList.classList.remove('active')
-                menuButton.classList.remove('active')
-            })
-        }
-        
-        eventos.forEach((userEvent) => {
-            menuButton.addEventListener(userEvent, openMenu)
-        })
-    }
-}
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const eventos = ['click', 'touchstart'];
 
+  if (menuButton) {
+    function openMenu(event) {
+      menuList.classList.add('active');
+      menuButton.classList.add('active');
+      outsideClick(menuList, eventos, () => {
+        menuList.classList.remove('active');
+        menuButton.classList.remove('active');
+      });
+    }
+
+    eventos.forEach((userEvent) => {
+      menuButton.addEventListener(userEvent, openMenu);
+    });
+  }
+}
