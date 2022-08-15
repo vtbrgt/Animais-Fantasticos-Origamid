caa8504f4a8282d4d845e713ca1cf3325c1a5474
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-08-15 15:07:25 -0300

    adding business hours

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js'
+import initAnimaNumeros from './modules/anima-numeros.js';
+import initFuncionamento from './modules/funcionamento.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,3 +18,4 @@ initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
+initFuncionamento();
