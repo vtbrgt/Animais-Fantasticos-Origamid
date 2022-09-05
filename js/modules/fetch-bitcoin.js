35f2f633bed5809074d93b62cb926a8d658065bc
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-09-05 11:13:29 -0300

    bug fix

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function initFetchBitcoin() {
   fetch('https://blockchain.info/ticker')
     .then((response) => response.json())
@@ -10,18 +9,3 @@ export default function initFetchBitcoin() {
       console.log(Error(erro));
     });
 }
-=======
-export default function initFetchBitcoin() {}
-
-fetch('https://blockchain.info/ticker')
-  .then((response) => response.json())
-  .then((bitcoin) => {
-    const btcPreco = document.querySelector('.btc-preco');
-    btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
-  })
-  .catch((erro) => {
-    console.log(Error(erro));
-  });
-
-// https://blockchain.info/ticker
->>>>>>> eee1a9bdb16c6f84808f7a2c888afec21ea553af
