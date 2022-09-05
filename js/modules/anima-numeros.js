14fe7dfc6c05ce5b81644cd8362d7260f383a11d
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-09-05 11:05:59 -0300

    correcting JS with eslint

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,34 +1,34 @@
 export default function initAnimaNumeros() {
-        function animaNumeros() {
-        const numeros = document.querySelectorAll('[data-numero]')
+  function animaNumeros() {
+    const numeros = document.querySelectorAll('[data-numero]');
 
-        numeros.forEach((numero) => {
-            const total = +numero.innerText;
-            const incremento = Math.floor(total / 100);
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const incremento = Math.floor(total / 100);
 
-            let start = 0;
-            const timer = setInterval(() => {
-                start = start + incremento;
-                numero.innerText = start;
-                if (start > total) {
-                    numero.innerText = total
-                    clearInterval(timer)
-                }
-            },25 * Math.random());
-        })    
-    }
-
-
-    function handleMutation(mutation) {
-        if (mutation[0].target.classList.contains('ativo')) {
-            observer.disconnect();
-            animaNumeros();
+      let start = 0;
+      const timer = setInterval(() => {
+        start += incremento;
+        numero.innerText = start;
+        if (start > total) {
+          numero.innerText = total;
+          clearInterval(timer);
         }
+      }, 25 * Math.random());
+    });
+  }
+
+  const observerTarget = document.querySelector('.numeros');
+  let observer;
+
+  observer.observe(observerTarget, { attributes: true });
+
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains('ativo')) {
+      observer.disconnect();
+      animaNumeros();
     }
+  }
 
-    const observerTarget = document.querySelector('.numeros')
-    const observer = new MutationObserver(handleMutation)
-
-    observer.observe(observerTarget, {attributes: true})
+  observer = new MutationObserver(handleMutation);
 }
-
